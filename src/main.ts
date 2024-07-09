10f0560c82de42473e55067e57b2d0823e6be332
Sylvain Leroux <sylvain@chicoree.fr>
2024-07-09 14:12:13 +0200

    WIP Query Gnosisscan

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,59 @@
+const MY_WALLET=process.env.MY_WALLET
+
+import fetch from 'node-fetch';
+import { URL, URLSearchParams } from 'node:url'; 
+
+const GNOSISSCAN_API_KEY=process.env.GNOSISSCAN_API_KEY
+const GNOSISSCAN_BASE_ADDRESS='https://api.gnosisscan.io/api'
+
+function gnosisAPIUrl(params) {
+  const url = new URL(GNOSISSCAN_BASE_ADDRESS);
+  const search_params = new URLSearchParams(params);
+  url.search = search_params.toString()
+
+  return url;
+}
+
+async function gnosisFetch(params) {
+  const url = gnosisAPIUrl(params);
+  const res = await fetch(url);
+
+  return await res.json();
+}
+
+async function gnosisAccountTransactions() {
+  const params = {
+    module: 'account',
+    action: 'txlist',
+    startBlock: 0,
+    endBlock: 99999999,
+    sort: 'asc',
+    address: MY_WALLET,
+    apiKey: GNOSISSCAN_API_KEY,
+  }
+  return await gnosisFetch(params)
+}
+
+async function gnosisAccountTokenTransfers() {
+  const params = {
+    module: 'account',
+    action: 'tokentx',
+    startBlock: 0,
+    endBlock: 99999999,
+    sort: 'asc',
+    address: MY_WALLET,
+    apiKey: GNOSISSCAN_API_KEY,
+  }
+  return await gnosisFetch(params)
+}
+
+async function testGnosisFetch() {
+  return await gnosisAccountTokenTransfers()
+}
+
 import { Client, fetchExchange } from '@urql/core';
 
 const THEGRAPH_API_KEY=process.env.THEGRAPH_API_KEY
-const MY_WALLET=process.env.MY_WALLET
 const QUERY = `
   fragment ReserveFields on Reserve {
     name
@@ -58,10 +110,12 @@ const client = new Client({
 
 async function main() {
 	console.dir(process.env);
-	const result = await client.query(QUERY, {
-		id: MY_WALLET,
-	});
-	console.dir(result, {depth: 5});
+//	const result = await client.query(QUERY, {
+//		id: MY_WALLET,
+//	});
+//        console.dir(result, {depth: 5});
+
+        console.log(await testGnosisFetch());
 }
 
 main()
